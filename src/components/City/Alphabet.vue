747812fd1715eef78c2b11376b29a4a175a3e0eb
lqh <515514053@qq.com>
2019-09-20 17:10:10 +0800

    city-list

---
+++ src/components/City/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="list">
+    <li class="item">a</li>
+    <li class="item">a</li>
+    <li class="item">a</li>
+    <li class="item">a</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Alphabet'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import "~@css/varibles.styl"
+  .list
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    right 0
+    top 1.78rem
+    bottom 0
+    width .4rem
+    .item
+      text-align center
+      line-height .4rem
+      color: $bgcolor
+</style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -2,15 +2,21 @@
   <div>
     <city-header></city-header>
     <search></search>
+    <list></list>
+    <alphabet></alphabet>
   </div>
 </template>
 
 <script>
 import CityHeader from '../components/City/CityHeader'
 import Search from '../components/City/Search'
+import List from '../components/City/List'
+import Alphabet from '../components/City/Alphabet'
 export default {
   name: 'City',
   components: {
+    Alphabet,
+    List,
     Search,
     CityHeader
   }
